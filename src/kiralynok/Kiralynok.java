376456bacef5364cd91f681213dd37fe2cfa3432
SiMate99 <sibakmate@gmail.com>
2021-02-17 10:26:11 +0100

    4. feladat kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 
 public class Kiralynok {
     public static void main(String[] args) throws IOException {
-        
+        System.out.println("4. feladat: Az üres tábla");
+        Tabla tabla = new Tabla('#');
+        tabla.megjelenit();
     }
     
 }
