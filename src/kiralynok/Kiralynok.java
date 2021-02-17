17a047fd2ff3f88ef8020ae5a2c215e3be4772d4
SiMate99 <sibakmate@gmail.com>
2021-02-17 10:49:22 +0100

    OOP 9. feladatig

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -19,18 +19,27 @@ public Kiralynok(){
     public void megoldasok(){
         feladat4();
         feladat6();
+        feladat9();
     }
 
     private void feladat4() {
+        //4. feladat
         System.out.println("4. feladat: Az üres tábla");
-        Tabla tabla = new Tabla('#');
         tabla.megjelenit();
     }
     
     private void feladat6() {
+        //6. feladat
         System.out.println("6. feladat: Feltöltött tábla:");
         tabla.elhelyez(8);
         tabla.megjelenit();
+    }
+
+    private void feladat9() {
+        //9. feladat
+        System.out.println("9. feladat: üres sorok és oszlopok száma:");
+        System.out.println("sorok száma: " + tabla.uresSorokSzama());
+        System.out.println("oszlopok száma: " + tabla.uresOszlopokSzama());
     }
     
 }
